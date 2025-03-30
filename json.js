54d3b2a0483ada865f9487bfec0d358b70d1e07a
elivandro <elivandro78@gmail.com>
2025-03-30 20:37:19 -0300

    criando o menu dropdow

---
+++ json.js
@@ -0,0 +1,13 @@
+export const pessoa = [
+    {
+        auth:"elivandro",
+        project: "Monitoramento",
+        tecnologia:"html e java"
+    },
+    {
+        auth:"elivandro",
+        project: "Chamados usuario",
+        tecnologia:"java e angular"
+    },
+    
+]
